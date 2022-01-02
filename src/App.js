891eb50a45919cd495f9d03045405b94d4449133
Md. Tajminur Rahman <tajminur.aiub@gmail.com>
2022-01-02 13:30:06 +0600

    auth, google sign in, firebase

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import AuthProvider from './context/AuthProvider';
 import Home from './Pages/Home/Home';
 import Jobs from './Pages/Jobs/Jobs';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
-
-      <Jobs></Jobs>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/login' element={<Login></Login>} />
+            <Route path='/' element={<Home></Home>} />
+            <Route path='/jobs' lement={<Jobs></Jobs>} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
 
   );
