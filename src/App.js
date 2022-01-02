b7c697c6e315005fe0da1362364a2628e684359c
Mostofareza3 <86896410+Mostofareza3@users.noreply.github.com>
2022-01-02 19:32:58 +0600

    job details

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 
 import './App.css';
+import JobFullDetails from './components/Job/JobFullDetails';
 import Home from './Pages/Home/Home';
 import Jobs from './Pages/Jobs/Jobs';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
-
-      <Jobs></Jobs>
+      <JobFullDetails/>
     </div>
 
   );
