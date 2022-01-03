45ab8e4f1f47750486663c7af3625519a38f0dd1
Md. Sanaul Islam <86657683+sanaul01@users.noreply.github.com>
2022-01-03 11:54:36 +0600

    update app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import AuthProvider from './context/AuthProvider';
 import Home from './Pages/Home/Home';
 import Jobs from './Pages/Jobs/Jobs';
 import Login from './Pages/Login/Login';
-import Profail from './Pages/Profail/Profail';
 import ProfailDrawer from './Pages/Profail/ProfailDrawer/ProfailDrawer';
 
 function App() {
@@ -17,8 +16,7 @@ function App() {
             <Route path='/login' element={<Login></Login>} />
             <Route path='/' element={<Home></Home>} />
             <Route path='/jobs' element={<Jobs></Jobs>} />
-            <Route path='/profail' element={<Profail></Profail>} />
-            <Route path='/Profaildrawer' element={<ProfailDrawer></ProfailDrawer>} />
+            <Route path='/my-profile' element={<ProfailDrawer></ProfailDrawer>} />
           </Routes>
         </BrowserRouter>
       </AuthProvider>
