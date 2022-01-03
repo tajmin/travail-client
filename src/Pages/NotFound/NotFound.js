175c07ee826c543d106b936105812eed5b9f1fc2
Imran Hossain <86704539+emranhasan518@users.noreply.github.com>
2022-01-03 23:37:31 +0600

    Update NotFound.js

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,20 +1,20 @@
 import { Button, Container, Grid } from '@mui/material';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Navbar from '../../components/Shared/Navbar';
 
 const NotFound = () => {
     return (
         <Grid>
             <Navbar></Navbar>
                 <Grid>
-                    <Grid style={{width:'50%', marginLeft:'25%'}}>
+                    <Grid style={{width:'40%', height:'20%', marginLeft:'30%', marginTop:'5px'}}>
                         <img style={{ width: "100%"}}  src="https://image.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-1932.jpg" />
                     </Grid>
                     <NavLink to="/" style={{textDecoration:'none'}}>
                         <Button variant="contained">Go Back</Button>
                     </NavLink>
                 </Grid>
-            <Footer></Footer>
         </Grid>
     );
 };
