e83e15d67ee8c5dc9486f843256bf2b1f72ab782
Md. Tajminur Rahman <tajminur.aiub@gmail.com>
2022-01-05 18:35:23 +0600

    thunk call in job details

--- src/redux/slices/jobSlice.js
+++ src/redux/slices/jobSlice.js
@@ -9,10 +9,20 @@ export const fetchAllJobs = createAsyncThunk(
     }
 )
 
+export const fetchJobDetails = createAsyncThunk(
+    'job/fetchJobDetails',
+    async (id) => {
+        const response = await fetch(`https://pacific-lowlands-19741.herokuapp.com/availableJobs/${id}`)
+            .then(res => res.json())
+        return response;
+    }
+)
+
 export const jobSlice = createSlice({
     name: 'job',
     initialState: {
         jobs: [],
+        jobDetails: {},
         isLoading: true
     },
     reducers: {
@@ -23,6 +33,15 @@ export const jobSlice = createSlice({
             state.isLoading = false;
         })
         builder.addCase(fetchAllJobs.pending, (state, action) => {
+            state.isLoading = true;
+        })
+        builder.addCase(fetchJobDetails.fulfilled, (state, action) => {
+            state.jobDetails = {};
+            console.log(action.payload);
+            state.jobDetails = action.payload;
+            state.isLoading = false;
+        })
+        builder.addCase(fetchJobDetails.pending, (state, action) => {
             state.isLoading = true;
         })
     },
